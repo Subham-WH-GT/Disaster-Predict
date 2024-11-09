2962612b6c090e3f0dcc9f5de56e5294a13a1a69
Subham-WH-GT <awandboy685@gmail.com>
2024-11-10 01:00:33 +0530

    updated

--- static/js/script.js
+++ static/js/script.js
@@ -6,7 +6,7 @@ document.getElementById('prediction-form').addEventListener('submit', async func
     const depth = parseFloat(document.getElementById('depth-input').value);
 
     try {
-        const response = await fetch('http://127.0.0.1:5000/predict', {
+        const response = await fetch('http://139.84.175.31/predict', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
